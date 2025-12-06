56cc56d210041553a04535ec0599da52b0c4b583
brxshncy <ggkidsbruce619@gmail.com>
2025-12-06 13:36:35 +0800

    add sidebar

--- frontend/components/layout/sidebar.tsx
+++ frontend/components/layout/sidebar.tsx
@@ -7,40 +7,45 @@ interface SidebarProps {
 
 export const Sidebar: React.FC<SidebarProps> = ({ navListItems }) => {
     return (
-       <aside className="h-screen w-[260px] bg-mid-night flex flex-col border-r border-mid-night/60 text-platinum py-8 px-6">
-            <div className="flex items-center gap-3 mb-6 cursor-pointer hover:bg-platinum/5 rounded p-1 transition-colors">
-                <div className="bg-platinum/10 flex items-center rounded-md p-2 text-xs">
+        <aside className="h-screen w-64 flex flex-col bg-mid-night text-platinum border-r border-r-mid-night/50 py-8 px-6">
+            <div className="flex gap-3 items-center mb-6 cursor-pointer hover:bg-platinum/5 rounded-md p-1 transition-colors">
+                <div className="flex items-center justify-center h-8 w-8 bg-platinum/10 rounded-lg text-xs font-medium">
                     DF
                 </div>
                 <div>
-                    <h2 className="font-medium text-lg tracking-tight">
-                        DeciFit
-                    </h2>
-                     <p className="text-slate-400 tracking-[0.25em] text-xs uppercase">
+                    <h4 className="text-lg font-bold tracking-tight">
+                        DefiFit
+                    </h4>
+                    <p className="text-slate-400 uppercase text-xs tracking-widest">
                         Admin
                     </p>
                 </div>
             </div>
-            <ul className="space-y-6 overflow-y-auto6">
-                {navListItems.map((navItem) => {
-                    return (
-                        <li key={navItem.title}>
-                            <p className="uppercase text-slate-500 text-xs tracking-widest">
-                                 {navItem.title}
-                            </p>
-                            <ul className="mt-1">
-                                {navItem.items.map((item) => {
-                                    return (
-                                        <li className="pl-3 text-slate-200 hover:text-white hover:bg-platinum/5 rounded cursor-pointer py-2 px-3 text-sm font-medium tracking-tight transition-colors">
-                                            {item.name}
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        </li>
-                    )
-                })}
-            </ul>
-       </aside>
+            <nav className="flex flex-col overflow-y-auto">
+                <ul className="space-y-6 flex-1">
+                    {navListItems.map((navItem) => {
+                        return (
+                            <li key={navItem.title}>
+                                <p className="text-slate-500 tracking-widest uppercase font-semibold text-xs">
+                                    {navItem.title}
+                                </p>
+                                <ul className="flex flex-col gap-2 mt-2">
+                                    {navItem.items.map((item) => {
+                                        return (
+                                            <li 
+                                                key={item.name}
+                                                className="text-slate-200 pl-5 cursor-pointer hover:text-white hover:bg-platinum/5 rounded-md text-sm py-1.5 pr-3 tracking-wider transition-colors"
+                                            >
+                                                {item.name}
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </li>
+                        )}
+                    )}
+                </ul>
+            </nav>
+        </aside>
     )
 }
